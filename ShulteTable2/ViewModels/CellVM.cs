068a63212f09e962ecf4fdf4b15e7bdba47cdd01
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-06-18 18:47:29 +0300

    Добавьте файлы проекта.

---
+++ ShulteTable2/ViewModels/CellVM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Input;
+using ShulteTable2.Helpers;
+namespace ShulteTable2.ViewModels
+{
+   
+    public enum CellColor
+    {
+        White, Green, Purple, Yellow, Blue, Red
+    };
+
+    public enum CellState
+    {
+        Current, IsWrong,Pressed
+    }
+
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class CellVM
+    {
+        private RelayCommand clickCommand;
+        private static Random rnd = new();
+        private Action<CellVM>? changed;
+
+        public int Number { get; }
+        public CellColor Color { get; set; }
+        public CellState State { get; set; }
+        public ICommand ClickCommand => clickCommand;
+    
+
+        public CellVM(int number) : this(number, GetRandomColor()) { }
+        public CellVM(int number,Action<CellVM> change) : this(number) {changed = change; }
+        public CellVM(int number, CellColor color)
+        {
+            Number = number;
+            Color = color;
+            State = CellState.IsWrong;
+            clickCommand = new((o) => mouseClick());
+        }
+
+        private static CellColor GetRandomColor() => (CellColor)rnd.Next(Enum.GetValues(typeof(CellColor)).Length);
+       
+        private void mouseClick()
+        {
+            if (State == CellState.Current)
+            {
+                State = CellState.Pressed;
+                changed?.Invoke(this);
+            }
+        }
+    }
+}
---
+++ ShulteTable2/ViewModels/TableVM.cs
@@ -0,0 +1,50 @@
+﻿using ShulteTable2.Helpers;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ShulteTable2.ViewModels
+{
+    public class TableVM
+    {
+        private ObservableCollection<CellVM> cells = new ();
+    
+        public IEnumerable<CellVM> Cells => cells;
+
+        public const int EYE_NUMBER = 0;
+        public int FirstNumber { get; } = 1;
+        public int LastNumber { get; }
+
+        public TableVM(int size)
+        {
+            LastNumber = size * size - 1;
+            GenerateCells();
+        }
+
+        private void GenerateCells()
+        {
+            if(cells.Count !=0)cells.Clear();
+            for (int i = FirstNumber; i <= LastNumber; ++i)
+                 cells.Add(new(i, cellChangeed));
+            cells[0].State = CellState.Current;
+            this.cells.Shuffle();
+            cells.Insert(cells.Count / 2, new CellVM(EYE_NUMBER, CellColor.White));
+        }
+
+        private void cellChangeed(CellVM cell)
+        {
+            cell.State = CellState.IsWrong;
+            CellVM? cNext = cells.FirstOrDefault(n => n.Number == cell.Number + 1);
+            if (cNext != null) cNext.State = CellState.Current;
+            else
+            {
+                MessageBox.Show("Done");
+                GenerateCells();
+            }
+        }
+    }
+}
